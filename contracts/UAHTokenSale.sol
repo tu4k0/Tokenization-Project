49599c7cf5150be50796b4bbd9c2d827f02b3837
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-05 20:35:59 +0300

    Create UAHTokenSale.sol
    Added smart contract for UAH tokensale - functionality about how/who/where can buy UAH tokens.

---
+++ contracts/UAHTokenSale.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.6.1;
+
+import "./Crowdsale.sol";
+
+contract UAHTokenSale is Crowdsale {
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+}
