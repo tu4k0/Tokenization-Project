59b5c6e37de82ebb9f8dd0f678638e6a495d45d8
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-05 19:01:31 +0300

    Update UAHToken.test.js
    Added 2 Unit test functions.

--- test/UAHToken.test.js
+++ test/UAHToken.test.js
@@ -12,12 +12,34 @@ const expect = chai.expect;
 
 contract("Token Test", async (accounts)=> {
 
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+    
     it("all tokens in my account", async () => {
         let instance = await UAHToken.deployed();
         let totalSupply = await instance.totalSupply();
         //let balance = await instance.balanceOf(accounts[0]);
         //assert.equal(balance.valueOf(), initialSupply.valueOf(), "balance not the same");
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    })
+
+    it ("is possible to send tokens between another accounts", async () => {
+
+        const sendTokens = 1;
+        let instance = await UAHToken.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    })
+
+    it ("is not possible to send more tokens than available", async () => {
+
+        
+        let instance = await UAHToken.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount); 
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
     })
 
 });
