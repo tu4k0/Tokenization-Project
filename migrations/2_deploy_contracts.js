09d3b173794050b58352c581410bf0facdda76fd
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-05 20:54:03 +0300

    Update 2_deploy_contracts.js
    Added UAHTokenSale artifact. Added deployment of UAHTokenSale in Ganache Blockchain and send all UAH tokens from owner to UAHTokensale contract.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-var UAHToken = artifacts.require("Token.sol");
+var UAHToken = artifacts.require("UAHToken.sol");
+var UAHTokenSale = artifacts.require("UAHTokenSale");
 
-module.exports = async function(deployer){
-    await deployer.deploy(UAHToken, 1000);
+module.exports = async function(deployer) {
+    
+    let addr = await web3.eth.getAccounts();
+    
+    await deployer.deploy(UAHToken,1000);
+    await deployer.deploy(UAHTokenSale, 1, addr[0], UAHToken.address);
+    let instance = await UAHToken.deployed();
+    instance.transfer(UAHTokenSale.address, 1000);
 }
