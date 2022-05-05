cfa4a04592242d368842017e3049011482285736
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-05 21:48:30 +0300

    Update 2_deploy_contracts.js
    Modified 100 => process.env.INITIAL_TOKENS, that set initial amount of UAH tokens that will be minting in smart contract creating.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,13 @@
 var UAHToken = artifacts.require("UAHToken.sol");
 var UAHTokenSale = artifacts.require("UAHTokenSale");
+require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) {
     
     let addr = await web3.eth.getAccounts();
     
-    await deployer.deploy(UAHToken,1000);
+    await deployer.deploy(UAHToken, process.env.INITIAL_TOKENS);
     await deployer.deploy(UAHTokenSale, 1, addr[0], UAHToken.address);
     let instance = await UAHToken.deployed();
-    instance.transfer(UAHTokenSale.address, 1000);
+    instance.transfer(UAHTokenSale.address, process.env.INITIAL_TOKENS);
 }
