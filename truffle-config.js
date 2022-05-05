96bc65ab68b0b6477698b9d931d953aeea45d364
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-05 18:38:19 +0300

    Update truffle-config.js
    Update truffle config for Chai Unit testing.

--- truffle-config.js
+++ truffle-config.js
@@ -5,13 +5,15 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      port: 7545,
+      host: "127.0.0.1",
+      network_id: 5777
     }
   },
   compilers: {
     solc: {
-      version: "0.6.0"
+      version: "0.6.1"
     }
   }
 };
