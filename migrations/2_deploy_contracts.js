6e6f64689c78c957182224574a74c7bd1fb9b44d
karili4 <91626915+tu4k0@users.noreply.github.com>
2022-05-06 11:51:39 +0300

    Update 2_deploy_contracts.js
    Added KYC artifacts and deployment of KYC smart contract.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var UAHToken = artifacts.require("UAHToken.sol");
 var UAHTokenSale = artifacts.require("UAHTokenSale");
+var UAHKYC = artifacts.require("KYC");
+
 require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) {
@@ -7,7 +9,8 @@ module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
     
     await deployer.deploy(UAHToken, process.env.INITIAL_TOKENS);
-    await deployer.deploy(UAHTokenSale, 1, addr[0], UAHToken.address);
+    await deployer.deploy(UAHKYC);
+    await deployer.deploy(UAHTokenSale, 1, addr[0], UAHToken.address, UAHKYC.address);
     let instance = await UAHToken.deployed();
     instance.transfer(UAHTokenSale.address, process.env.INITIAL_TOKENS);
 }
