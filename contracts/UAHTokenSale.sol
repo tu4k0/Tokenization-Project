1caef54c81b6acafed009ced519edb55e67be041
karili4 <91626915+tu4k0@users.noreply.github.com>
2022-05-06 11:50:40 +0300

    Update UAHTokenSale.sol
    Added KYC settings for UAH tokensale.

--- contracts/UAHTokenSale.sol
+++ contracts/UAHTokenSale.sol
@@ -1,16 +1,25 @@
 pragma solidity 0.6.1;
 
 import "./Crowdsale.sol";
+import "./KYC.sol";
 
 contract UAHTokenSale is Crowdsale {
+
+    KYC kyc;
     constructor(
         uint256 rate,    // rate in TKNbits
         address payable wallet,
-        IERC20 token
+        IERC20 token,
+        KYC _kyc
     )
         Crowdsale(rate, wallet, token)
         public
     {
-
+        kyc = _kyc;
     }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(kyc.KYCCompleted(msg.sender), "KYC not completed, denied!");
+       }
 }
